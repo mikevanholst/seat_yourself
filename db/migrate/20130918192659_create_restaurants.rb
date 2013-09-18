b23ec264687132fea17e513d4a83c8a46f915dc7
Michael van Holst <mikevanholst@gmail.com>
2013-09-18 15:31:33 -0400

    adds restaurant model

---
+++ db/migrate/20130918192659_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.text :address
+      t.string :price_range
+      t.string :neighbourhood
+      t.integer :seats
+      t.text :description
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
