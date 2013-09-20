0a8f602cf3586eb9cf73b1828c2237032222a3f9
Michael van Holst <mikevanholst@gmail.com>
2013-09-19 21:20:49 -0400

    recreates schema

--- db/migrate/20130919202123_create_reservations.rb
+++ db/migrate/20130919202123_create_reservations.rb
@@ -3,6 +3,8 @@ class CreateReservations < ActiveRecord::Migration
     create_table :reservations do |t|
       t.integer :party_size
       t.datetime :time_slot
+      t.belongs_to :user
+      t.belongs_to :restaurant
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20130919202123) do
   create_table "reservations", force: true do |t|
     t.integer  "party_size"
     t.datetime "time_slot"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
